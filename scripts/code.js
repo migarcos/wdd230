89373df50ced6c12026ca9cce8b18355793b9949
Miguel Arcos <migark@hotmail.com>
2023-04-19 19:56:30 -0500

    fst modification

---
+++ scripts/code.js
@@ -0,0 +1,10 @@
+let lastupd = new Date();
+
+const mySuperDuperDate = new Date(document.lastModified);
+const mySuperDuperYear = mySuperDuperDate.getFullYear();
+const mySuperDuperHours = ('0' + mySuperDuperDate.getHours()).slice(-2);
+const mySuperDuperMinutes = ('0' + mySuperDuperDate.getMinutes()).slice(-2);
+const mySuperDuperSeconds = ('0' + mySuperDuperDate.getSeconds()).slice(-2);
+
+// document.getElementById('copyrightYear').innerHTML = mySuperDuperYear
+document.getElementById('lastUpdate').innerHTML = `${mySuperDuperDate.getDate()}/${mySuperDuperDate.getMonth()}/${mySuperDuperDate.getFullYear()} ${mySuperDuperHours}:${mySuperDuperMinutes}:${mySuperDuperSeconds}`
