e8369cd9e7581b2e915378e139035de35e06ed20
Miguel Arcos <migark@hotmail.com>
2023-06-03 06:19:43 -0500

    lesson 7 updated

--- lesson7/scripts/script.js
+++ lesson7/scripts/script.js
@@ -1,41 +1,49 @@
-const imagesToLoad = document.querySelectorAll("img[data-src]")
+// FIRST Easy way all images at time
+// - - - - - - - - - - - - - - - - - - 
+// let imagesToLoad = document.querySelectorAll("img[data-src]");
+//
+// const loadImages = (image) => {
+//   image.setAttribute("src", image.getAttribute("data-src"));
+//   image.onload = () => {
+//     image.removeAttribute("data-src");
+//   };
+// };
 
-let callback = (entries, observer) => {
-    entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-            let elem = entry.target;
-      
-            if (entry.intersectionRatio >= 0.75) {
-              intersectionCounter++;
-            }
-        }
-    });
+// imagesToLoad.forEach((img) => {
+//     loadImages(img);
+//   });
+// - - - - - - - - - - - - - - - - - - 
+
+// SECOND way (load target images only when the user scrolls down)
+let imagesToLoad = document.querySelectorAll("img[data-src]");
+
+const loadImages = (image) => {
+  image.setAttribute("src", image.getAttribute("data-src"));
+  image.onload = () => {
+    image.removeAttribute("data-src");
+  };
 };
-  
-let options = {
-    root: document.querySelector("#scrollArea"),
-    rootMargin: "0px",
-    threshold: 1.0,
-};
-  
-let observer = new IntersectionObserver(callback, options);
-  
+
+imagesToLoad.forEach((img) => {
+    loadImages(img);
+  })
+
 
 if ("IntersectionObserver" in window) {
     const observer = new IntersectionObserver((items, observer) => {
-        items.forEach((item) => {
+      items.forEach((item) => {
         if (item.isIntersecting) {
-            loadImages(item.target);
-            observer.unobserve(item.target);
+          loadImages(item.target);
+          observer.unobserve(item.target);
         }
-        });
+      });
     });
     imagesToLoad.forEach((img) => {
       observer.observe(img);
     });
-} else {
+  } else {
     imagesToLoad.forEach((img) => {
-        loadImages(img);
+      loadImages(img);
     });
-}
+  }
   
